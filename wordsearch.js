f4533e1fe365b518820841b1296eb647db236541
munraj <munrajgrewal12@gmail.com>
2021-01-14 01:05:11 +0000

    working word search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,31 @@
 const wordSearch = (letters, word) => { 
+
+    let vertical = [];
+    for (let i = 0; i < letters[0].length; i++) {
+        vertical.push([]);
+    }
+    for (let i = 0; i < letters.length; i++) { 
+        for (let j = 0; j < vertical.length; j++) {
+            vertical[j].push(letters[i][j]);
+        }
+    }
+    
+    const verticalJoin = vertical.map(ls => ls.join(''))
+    for (l of verticalJoin) {
+        if (l.includes(word)) {
+            return true;
+        }
+    }
+    
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)) {
+            return true;
+        }
     }
+
+    return false;
+
 }
 
 module.exports = wordSearch
